c67d4627d706a749839c837a36cf52788b468aeb
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-08-11 14:43:41 +0400

    Update iCanWin.test.js

--- iCanWin.test.js
+++ iCanWin.test.js
@@ -6,24 +6,18 @@ import ChromeDriverService from 'wdio-chromedriver-service';
 
 describe('Pastebin task', () => {
     it('Paste start', async () => {
-        await PastePage.open('https://pastebin.com');
-        
-         await PastePage.textName.setValue('helloweb');
-         await PastePage.textPasta.setValue("Привет от WebDriver");
-
-         const elem = await $('//*[@id="select2-postform-expiration-container"]');
-         await elem.waitForClickable({ timeout: 6000 });
-         
-           //const el10 = await  $('select2-postform-expiration-result-du2a-10M');
-           //await el10.waitForClickable({timeout: 6000}); 
-           await $(`#select2-postform-expiration-container`).click();
-           let neededExpiration = await $(`#select2-postform-expiration-results :nth-child(3)`);
-           await neededExpiration.waitForExist();
-           await neededExpiration.click();
-
-           const Button = await $('button[type="submit"]');
-           await Button.waitForClickable({timeout:6000});
-           })
+    await PastePage.open('https://pastebin.com');
+    await PastePage.textName.setValue('helloweb');
+    await PastePage.textPasta.setValue("Привет от WebDriver");
+    const elem = await $('//*[@id="select2-postform-expiration-container"]');
+    await elem.waitForClickable({ timeout: 6000 });
+    await $(`#select2-postform-expiration-container`).click();
+    let neededExpiration = await $(`#select2-postform-expiration-results :nth-child(3)`);
+    await neededExpiration.waitForExist();
+    await neededExpiration.click();
+    const Button = await $('button[type="submit"]');
+    await Button.waitForClickable({timeout:6000});
+    })
     });
     
 
