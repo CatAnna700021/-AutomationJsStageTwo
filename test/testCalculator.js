53c7fd624c9cb1f7657ae9190ca7f8c708138a28
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-09-15 14:59:19 +0400

    Create testCalculator.js

---
+++ test/testCalculator.js
@@ -0,0 +1,27 @@
+const Calculator = require('../app/Calculator');
+const { expect } = require('chai');
+let calculator = new Calculator();
+
+describe('add positive scenarios', () => {
+    it('should return sum of firstNumber and secondNumber', () => {
+      expect(calculator.add(2, 3)).to.be.equal(5);
+    });
+  });
+
+  describe('add negative scenarios', () => {
+    it('should throw exception if one of arguments is not a number', () => {
+      expect(() => calculator.add(2, 'hello')).to.throw(Error,'You should use numbers for adding.');
+    });
+  });
+
+  describe('add positive scenarios', () => {
+    it('should return result of multiplying firtNumber to secondNumber', () => {
+      expect(calculator.multiply(2, 3)).to.be.equal(6);
+    });
+  });
+
+  describe('add negative scenarios', () => {
+    it('should throw exception if one of arguments is not a number', () => {
+      expect(() => calculator.multiply(2, 'hello')).to.throw(Error,'You should use numbers for multiplying');
+    });
+  });
