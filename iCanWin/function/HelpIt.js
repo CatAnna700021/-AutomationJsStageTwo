4dca3f43a4d952c1928071be381e46b806dfe967
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-09-15 23:10:03 +0400

    Create HelpIt.js

---
+++ iCanWin/function/HelpIt.js
@@ -0,0 +1,25 @@
+import Page from '../test/pageObjects/page';
+
+class HelpIt extends Page {
+    async click(element){
+        await element.waitForExist(5000);
+        await element.click();
+    }
+    async write(element, text){
+        await element.waitForExist(5000);
+        await element.addValue(text);
+    }
+    async choseFromDroplist(droplist, element){
+        await droplist.waitForExist(5000);
+        await droplist.click();
+        await element.waitForExist(5000);
+        await element.click();
+    }
+    async checkTitle(element, text){
+        await element.waitForDisplayed(5000);
+        await expect(browser).toHaveTitleContaining(text);
+    }
+
+}
+
+module.exports = new HelpIt();
