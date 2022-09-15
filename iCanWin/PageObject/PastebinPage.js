78a4a8c6e85dc94e9af137cc1c90dabcf6f47063
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-09-15 23:05:27 +0400

    Add files via upload

---
+++ iCanWin/PageObject/PastebinPage.js
@@ -0,0 +1,18 @@
+import Page from './page';
+
+class PastebinPage extends Page {
+  get pasteForm() {return $('//*[@id="postform-text"]');}
+  get expirationList() {return $('//*[@id="select2-postform-expiration-container"]');}
+  get neededExpiration() {return $('//*[@id="select2-postform-expiration-results"]/li[3]');}
+  get pasteNameElement() {return $('//input[@id="postform-name"]');}
+  get createPasteBtn() {return $('//button[@type="submit"]');}
+  get pasteTextElement(){return $('//textarea[@class="textarea"]');}
+
+  get pasteText(){return 'Hello from WebDriver';}
+  get pasteName(){return 'helloweb';}
+
+  open() {
+    super.open('http://pastebin.com');
+  }
+}
+export default new PastebinPage();
