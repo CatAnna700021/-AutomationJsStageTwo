b45f02e2f59c9c354db80221a88ac0ec6a9f4ec9
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-09-19 17:24:57 +0400

    Add files via upload

---
+++ Hurt Me Plenty/test/PageObjects/EstimatePage.js
@@ -0,0 +1,12 @@
+import Page from "./Page";
+
+class EstimatePage extends Page{
+    get vmClassSelected() {return $('#compute > md-list > md-list-item:nth-child(8) > div');}
+    get instanceTypeSelected() {return $('//*[@id="compute"]/md-list/md-list-item[5]/div[1]');}
+    get localSDDSelected() {return $('//*[@id="compute"]/md-list/md-list-item[8]/div[2]');}
+    get regionSelected() {return $('#compute > md-list > md-list-item:nth-child(2) > div');}
+    get commitmentTermSelected() {return $('#compute > md-list > md-list-item:nth-child(6) > div');}
+    get estimatedCost() {return $('#resultBlock > md-card > md-card-content > div > div > div > h2 > b');}
+  }
+
+  module.exports = new EstimatePage();
