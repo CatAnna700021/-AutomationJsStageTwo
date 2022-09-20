dec89f463d7f67b9f2beb13c1624c93858fdcc20
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-09-20 15:18:00 +0400

    Add files via upload

---
+++ Hardcore/test/spec/cloudgoogle.js
@@ -0,0 +1,28 @@
+import CloudgooglePage from '../pageobjects/cloudgoogle.page';
+import CalculatorPageFn from '../../functions/calculatorPageFn';
+import YopmailPage from '../pageobjects/yopmail.page';
+
+describe('Hardcore task', () => {
+
+  it('shold open searchbox and find Pricing calculator', async () => {
+    await CloudgooglePage.openPricingCalc();
+    await CloudgooglePage.checkTitle();
+  });
+  it('should chose machine parameters', async () => {
+    await CalculatorPageFn.switchToCalculatorFrame();
+    await CalculatorPageFn.choseMachine();
+    await CalculatorPageFn.checkAddingResults();
+  });
+  it('should open second page with mailbox and generate new mail', async () => {
+    await CloudgooglePage.clickToEmailEstimate();
+    await YopmailPage.generateMail();
+  });
+  it('should copy and paste generated mail', async () => {
+    await CalculatorPageFn.copyGeneratedMail();
+    await YopmailPage.switchToYopmail()
+  });
+  it('should switch to mail page and check costs', async () => {
+    await YopmailPage.openMailedPrice();
+    await YopmailPage.checkMailedPrice();
+  });
+});
