1ea29e6dd7d17d4c2b6706901b3034adf83b015d
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-09-21 11:34:34 +0400

    Update EstimatePage.js

--- Hurt Me Plenty/test/PageObjects/EstimatePage.js
+++ Hurt Me Plenty/test/PageObjects/EstimatePage.js
@@ -6,7 +6,7 @@ class EstimatePage extends Page{
     get localSDDSelected() {return $('//*[@id="compute"]/md-list/md-list-item[8]/div[2]');}
     get regionSelected() {return $('#compute > md-list > md-list-item:nth-child(2) > div');}
     get commitmentTermSelected() {return $('#compute > md-list > md-list-item:nth-child(6) > div');}
-    get estimatedCost() {return $('#resultBlock > md-card > md-card-content > div > div > div > h2 > b');}
+    get estimatedCost() {return $('//div[@class="md-list-item-text"]/b');}
   }
 
   module.exports = new EstimatePage();
