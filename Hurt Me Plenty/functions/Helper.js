119d73029e3b77c458e69f7e01952d733edc8d09
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-10-05 11:19:36 +0400

    Update Helper.js

--- Hurt Me Plenty/functions/Helper.js
+++ Hurt Me Plenty/functions/Helper.js
@@ -3,30 +3,23 @@ import Page from '../test/pageObjects/Page';
 class Helper extends Page {
     async click(element){
      await element.waitForExist(5000);
-     await element.click();
-    }
+     await element.click();}
     async write (element, text){
     await element.waitForExist(5000);
-    await element.addValue(text);
-    }
+    await element.addValue(text);}
     async writeAndSumbit(element, text){
         await element.waitForExist(5000);
         await element.addValue(text);
-        await element.keys('\uE007');
-    }
+        await element.keys('\uE007');}
     async choseDroplistElement(droplist, element){
         await droplist.waitForExist(5000);
         await droplist.click();
         await element.waitForExist(5000);
-        await element.click();
-    }
+        await element.click();}
     async checkTitle(text){
-        await expect(browser).toHaveTitleContaining(text);
-    }
+        await expect(browser).toHaveTitleContaining(text);}
     async checkElementForHaving(element, text){
         await element.waitForExist(5000);
-        await expect(element).toHaveTextContaining(text);
-    }
-}
+        await expect(element).toHaveTextContaining(text);}}
 module.exports = new Helper();
 
