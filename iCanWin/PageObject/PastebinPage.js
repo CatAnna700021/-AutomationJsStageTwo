82c39def4ee6816322f5b4c29c590e33e4961d38
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-10-05 10:59:36 +0400

    Update PastebinPage.js

--- iCanWin/PageObject/PastebinPage.js
+++ iCanWin/PageObject/PastebinPage.js
@@ -7,12 +7,10 @@ class PastebinPage extends Page {
   get pasteNameElement() {return $('//input[@id="postform-name"]');}
   get createPasteBtn() {return $('//button[@type="submit"]');}
   get pasteTextElement(){return $('//textarea[@class="textarea"]');}
-
   get pasteText(){return 'Hello from WebDriver';}
   get pasteName(){return 'helloweb';}
 
   open() {
-    super.open('http://pastebin.com');
-  }
+    super.open('http://pastebin.com');}
 }
 export default new PastebinPage();
