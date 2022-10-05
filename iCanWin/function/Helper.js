9979f639e7a30fcb63b8e446ebe45300cb8f9742
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-10-05 11:00:42 +0400

    Update Helper.js

--- iCanWin/function/Helper.js
+++ iCanWin/function/Helper.js
@@ -3,23 +3,17 @@ import Page from '../test/pageObjects/Page';
 class Helper extends Page {
     async click(element){
         await element.waitForExist(5000);
-        await element.click();
-    }
+        await element.click();}
     async write(element, text){
         await element.waitForExist(5000);
-        await element.addValue(text);
-    }
+        await element.addValue(text);}
     async choseFromDroplist(droplist, element){
         await droplist.waitForExist(5000);
         await droplist.click();
         await element.waitForExist(5000);
-        await element.click();
-    }
+        await element.click(); }
     async checkTitle(element, text){
         await element.waitForDisplayed(5000);
-        await expect(browser).toHaveTitleContaining(text);
-    }
-
+        await expect(browser).toHaveTitleContaining(text);}
 }
-
 module.exports = new Helper();
