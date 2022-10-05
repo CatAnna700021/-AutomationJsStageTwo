c465c849fb182895741a13b5a591867b17b83721
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-10-05 11:35:38 +0400

    Update testCalculator.js

--- test/testCalculator.js
+++ test/testCalculator.js
@@ -4,24 +4,20 @@ let calculator = new Calculator();
 
 describe('add positive scenarios', () => {
     it('should return sum of firstNumber and secondNumber', () => {
-      expect(calculator.add(2, 3)).to.be.equal(5);
-    });
+      expect(calculator.add(2, 3)).to.be.equal(5);});
   });
 
   describe('add negative scenarios', () => {
     it('should throw exception if one of arguments is not a number', () => {
-      expect(() => calculator.add(2, 'hello')).to.throw(Error,'You should use numbers for adding.');
-    });
+      expect(() => calculator.add(2, 'hello')).to.throw(Error,'You should use numbers for adding.');});
   });
 
   describe('add positive scenarios', () => {
     it('should return result of multiplying firtNumber to secondNumber', () => {
-      expect(calculator.multiply(2, 3)).to.be.equal(6);
-    });
+      expect(calculator.multiply(2, 3)).to.be.equal(6);});
   });
 
   describe('add negative scenarios', () => {
     it('should throw exception if one of arguments is not a number', () => {
-      expect(() => calculator.multiply(2, 'hello')).to.throw(Error,'You should use numbers for multiplying');
-    });
+      expect(() => calculator.multiply(2, 'hello')).to.throw(Error,'You should use numbers for multiplying');});
   });
