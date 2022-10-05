ec8df4772ede93307e3ab71026c95e942a832c41
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-10-05 11:29:04 +0400

    Update EstimatePage.js

--- Hardcore/test/pageObjects/EstimatePage.js
+++ Hardcore/test/pageObjects/EstimatePage.js
@@ -3,6 +3,5 @@ import Page from './page';
 class EstimatePage extends Page{
   get emailEstimateBtn() {return $('#email_quote');}
   get emailField() {return $('//input[@type="email"]');}
-  get sendEmailBtn() {return $('//button[@aria-label="Send Email"]');}
-}
+  get sendEmailBtn() {return $('//button[@aria-label="Send Email"]');}}
 module.exports = new EstimatePage();
