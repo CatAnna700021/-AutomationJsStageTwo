4a4b265f67b2dea9d064ca4ca86a5271b21fefe3
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-10-05 11:23:14 +0400

    Update coudgoogle.js

--- Hurt Me Plenty/test/specs/coudgoogle.js
+++ Hurt Me Plenty/test/specs/coudgoogle.js
@@ -5,16 +5,13 @@ import EstimatePageFn from "../../functions/EstimatePageFn";
 describe('Hurt me plenty task',()=>{
     it('shold open searchbox and find Pricing calculator', async () => {
         await CloudgooglePage.openPricingCalc();
-        await CloudgooglePage.checkTitle();
-      });
+        await CloudgooglePage.checkTitle();});
       it('should add machine with known parameters', async () => {
         await CalculatorPageFn.switchToCalculatorFrame();
         await CalculatorPageFn.choseMachine();
-        await CalculatorPageFn.checkAddingResults();
-      });
+        await CalculatorPageFn.checkAddingResults();});
       it('should check machine parameters and price', async () => {
         await EstimatePageFn.checkMachineParameters();
-        await EstimatePageFn.checkEstimatedCost();
-      });
+        await EstimatePageFn.checkEstimatedCost();});
     });
     
