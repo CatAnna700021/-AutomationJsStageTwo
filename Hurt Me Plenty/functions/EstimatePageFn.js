0cd150595995fff17311ed5e82fa54e49720b410
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-10-05 11:18:33 +0400

    Update EstimatePageFn.js

--- Hurt Me Plenty/functions/EstimatePageFn.js
+++ Hurt Me Plenty/functions/EstimatePageFn.js
@@ -4,29 +4,21 @@ import Helper from './Helper';
 
 class EstimatePageFn extends Page{
     async checkVMClass() {
-        await Helper.checkElementForHaving(EstimatePage.vmClassSelected, 'regular');
-      }
+        await Helper.checkElementForHaving(EstimatePage.vmClassSelected, 'regular');}
       async checkInstanceType() {
-        await Helper.checkElementForHaving(EstimatePage.instanceTypeSelected, 'n1-standard-8');
-      }
+        await Helper.checkElementForHaving(EstimatePage.instanceTypeSelected, 'n1-standard-8');}
       async checkLocalSDD() {
-        await Helper.checkElementForHaving(EstimatePage.localSDDSelected,'Local SSD: 2x375 GiB');
-      }
+        await Helper.checkElementForHaving(EstimatePage.localSDDSelected,'Local SSD: 2x375 GiB');}
       async checkRegion() {
-        await Helper.checkElementForHaving(EstimatePage.regionSelected,'Region: Frankfurt');
-      }
+        await Helper.checkElementForHaving(EstimatePage.regionSelected,'Region: Frankfurt');}
       async checkCommitmentTerm() {
-        await Helper.checkElementForHaving(EstimatePage.commitmentTermSelected,'Commitment term: 1 Year');
-      }
+        await Helper.checkElementForHaving(EstimatePage.commitmentTermSelected,'Commitment term: 1 Year');}
       async checkEstimatedCost() {
-        await Helper.checkElementForHaving(EstimatePage.estimatedCost,'Total Estimated Cost: USD 5,411.26 per 1 month');
-      }
+        await Helper.checkElementForHaving(EstimatePage.estimatedCost,'Total Estimated Cost: USD 5,411.26 per 1 month');}
       async checkMachineParameters(){
           await this.checkVMClass();
           await this.checkInstanceType();
           await this.checkLocalSDD();
           await this.checkRegion();
-          await this.checkCommitmentTerm();
-      }
-    }
+          await this.checkCommitmentTerm();}}
     module.exports = new EstimatePageFn();
