888d9632b5858bc48ba169de37d4258c83b3c21c
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-10-09 20:09:17 +0400

    Update task4.spec.js

--- specs/test/task4.spec.js
+++ specs/test/task4.spec.js
@@ -1,22 +1,22 @@
-const { default: GoogleCloudPage } = require('../../app/GoogleCloudPage');
+const GoogleCloudPage = require('../../app/GoogleCloudPage');
 const Page = require('../../app/Page');
-const { default: YopMailPage } = require('../../app/YopMailPage');
+const YopMailPage = require('../../app/YopMailPage');
 
-discribe('Hardcore', () =>{
-    before('Create a new calculator', async() =>{
-        await CloudgooglePage.open();
-        await CloudgooglePage.openPricingCalculator();
-        await CloudgooglePage.witchToCalculatorFrame();
-        await CloudgooglePage.addNewCalculator();
-        await CloudgooglePage.clickAddToEstimateBtn();  
-        await YopMailPage.generateMail();
-        await CloudgooglePage.sendEmail();
-        await YopMailPage.switchToYopmail();
-        await YopMailPage.penMailedPrice();
-        
-    });
- 
-    it('Check Total Estimated Monthly Cost ', async () => {
-        expect(GoogleCloudPage.estimatedCost).toHaveTextContaining(YopMailPage.monthlyCostsField);
-    });
+discribe('Hardcore', () => {
+  before( async () => {
+    await CloudgooglePage.open();
+    await CloudgooglePage.openPricingCalculator();
+    await CloudgooglePage.witchToCalculatorFrame();
+    await CloudgooglePage.addNewCalculator();
+    await CloudgooglePage.clickAddToEstimateBtn();
+    await YopMailPage.generateMail();
+    await CloudgooglePage.sendEmail();
+    await YopMailPage.switchToYopmail();
+    await YopMailPage.penMailedPrice();
+
+  });
+
+  it('Check Total Estimated Monthly Cost ', async () => {
+    expect(YopMailPage.monthlyCostsField).toHaveTextContaining(GoogleCloudPage.estimatedCost);
+  });
 });
