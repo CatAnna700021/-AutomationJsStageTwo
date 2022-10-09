ca594e94b96ea41117e0f955d7469a1d98a59fdb
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-10-09 19:42:26 +0400

    Delete iCanWin directory

--- iCanWin/specs/pastebin.js
+++
@@ -1,9 +0,0 @@
-import PastebinPageFn from '../../functions/PastebinFn';
-
-describe('I can win webdriverIO task', () => {
-
-  it('should create new paste', async () => {
-    await PastebinPageFn.createPaste();
-    await PastebinPageFn.checkPasteTitle();
-  });
-});
