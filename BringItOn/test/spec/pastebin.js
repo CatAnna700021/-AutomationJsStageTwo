922052cd6782f8c8e1c2840308513bf2cef7c946
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-10-09 19:41:56 +0400

    Delete BringItOn directory

--- BringItOn/test/spec/pastebin.js
+++
@@ -1,7 +0,0 @@
-import PastebinPageFn from '../../functions/PastebinFn';
-
-describe('Bring it on webdriverIO task', ()=>{
-    if ('should create new paste', async()=>{
-        await PastebinPageFn.createPaste();
-        await PastebinPageFn.checkPaste();});
-});
